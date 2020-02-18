4ad36cdfee3dbe0a4308ead22e40c5c7d78fff31
Luiz Henrique <luiz.sousa@samsung.com>
2020-02-18 11:28:51 -0400

    [iOS] Initial iOS Project

---
+++ Heroes/Heroes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heroes
+//
+//  Created by Luiz Henrique de Sousa on 18/02/20.
+//  Copyright © 2020 Luiz Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
